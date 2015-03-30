ca9f1babbb2bed7f27047eaa984a5e546ec758c4
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-30 15:12:42 -0700

    Fixed a bug that prevented the script from running properly if rs_timeout was not set.

--- API-1.5/right_api_client/interactive-right_api-console.rb
+++ API-1.5/right_api_client/interactive-right_api-console.rb
@@ -7,11 +7,11 @@ require 'irb'
 email = ENV['rs_email'] || 'your@email.com'     # RS User Account
 pass = ENV['rs_pswd'] || 'yourpassword'         # RS User Password
 acct_id = ENV['rs_acct'] || '12345'             # RS Account to Backup
-timeout = ENV['rs_timeout'].to_i || 60          # Timeout in Seconds
+timeout = ENV['rs_timeout'] || 60          # Timeout in Seconds
 
 
 # Authenticate
-@client = RightApi::Client.new(:email => email, :password => pass, :account_id => acct_id, :timeout => timeout)
+@client = RightApi::Client.new(:email => email, :password => pass, :account_id => acct_id, :timeout => timeout.to_i)
 puts "Authentication successful!  The RightAPI::Client object is @client"
 puts "Starting IRB console.  Type `exit` to terminate the console and return to the shell prompt."
 
