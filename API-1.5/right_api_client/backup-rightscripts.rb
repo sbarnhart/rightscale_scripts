eacea92c386a9538f20bd24ed86690594e0b0bc2
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-31 10:05:06 -0700

    Added some output to the right_api_client scripts that will identify whether or not the ENV variables are being used.  Some day there will be an elegant way to get the user input if its not selected... but not today...

--- API-1.5/right_api_client/backup-rightscripts.rb
+++ API-1.5/right_api_client/backup-rightscripts.rb
@@ -4,19 +4,28 @@ require 'rubygems'
 require 'right_api_client'
 
 
-email = ENV['rs_email'] || 'your@email.com'     # RS User Account
-pass = ENV['rs_pswd'] || 'yourpassword'         # RS User Password
-acct_id = ENV['rs_acct'] || '12345'             # RS Account to backup scripts from
-timeout = ENV['rs_timeout'] || 60               # Timeout in Seconds
-backup_location = ENV['rs_backup_loc'] || '.'   # Backup location - NO trailing slash
+
+# Primary Inputs
+email = ENV['rs_email'] || 'your@email.com'          # RS User Account
+pass = ENV['rs_pswd'] || 'yourpassword'              # RS User Password
+acct_id = ENV['rs_acct'] || '12345'                  # RS Account to backup scripts from
 
 
-puts "Backing up RightScripts using user: #{email} for RightScale Account: #{acct_id} to Location: #{backup_location}"
+# Optional Inputs
+timeout = ENV['rs_timeout'] || 60                    # Timeout in Seconds
+backup_location = ENV['rs_backup_location'] || '.'   # Backup location - NO trailing slash
+
+puts ENV.has_key?('rs_email') ? 'rs_email set by ENVIRONMENT' : 'rs_email set by SCRIPT [default]'
+puts ENV.has_key?('rs_pswd') ? 'rs_pswd set by ENVIRONMENT' : 'rs_pswd set by SCRIPT [default]'
+puts ENV.has_key?('rs_acct') ? 'rs_acct set by ENVIRONMENT' : 'rs_acct set by SCRIPT [default]'
+
 
 # Authenticate
 @client = RightApi::Client.new(:email => email, :password => pass, :account_id => acct_id, :timeout => timeout.to_i )
 
 puts "Authenticated!"
+
+puts "Backing up RightScripts using user: #{email} for RightScale Account: #{acct_id} to Location: #{backup_location}"
 
 @client.right_scripts.index.each do |rs|
   href = "http://my.rightscale.com#{rs.href}/source"
--- API-1.5/right_api_client/interactive-right_api-console.rb
+++ API-1.5/right_api_client/interactive-right_api-console.rb
@@ -4,15 +4,28 @@ require 'rubygems'
 require 'right_api_client'
 require 'irb'
 
-email = ENV['rs_email'] || 'your@email.com'     # RS User Account
-pass = ENV['rs_pswd'] || 'yourpassword'         # RS User Password
-acct_id = ENV['rs_acct'] || '12345'             # RS Account to Backup
-timeout = ENV['rs_timeout'] || 60               # Timeout in Seconds
+
+# Primary Inputs
+email = ENV['rs_email'] || 'your@email.com'          # RS User Account
+pass = ENV['rs_pswd'] || 'yourpassword'              # RS User Password
+acct_id = ENV['rs_acct'] || '12345'                  # RS Account to login to
+
+
+# Optional Inputs
+timeout = ENV['rs_timeout'] || 60                    # Timeout in Seconds
+
+
+puts ENV.has_key?('rs_email') ? 'rs_email set by ENVIRONMENT' : 'rs_email set by SCRIPT [default]'
+puts ENV.has_key?('rs_pswd') ? 'rs_pswd set by ENVIRONMENT' : 'rs_pswd set by SCRIPT [default]'
+puts ENV.has_key?('rs_acct') ? 'rs_acct set by ENVIRONMENT' : 'rs_acct set by SCRIPT [default]'
 
 
 # Authenticate
-@client = RightApi::Client.new(:email => email, :password => pass, :account_id => acct_id, :timeout => timeout.to_i)
-puts "Authentication successful!  The RightAPI::Client object is @client"
-puts "Starting IRB console.  Type `exit` to terminate the console and return to the shell prompt."
+@client = RightApi::Client.new(:email => email, :password => pass, :account_id => acct_id, :timeout => timeout.to_i )
+
+puts "Authenticated!"
+
+puts "The RightAPI::Client object is @client.  Starting IRB console......"
+puts "Type `exit` to terminate the console and return to the shell prompt."
 
 IRB.start
