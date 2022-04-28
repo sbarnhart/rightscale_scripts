31ded5483a5098e174bee044600fab7e10b6ce27
stevebarnhart <sbarnhart@flexera.com>
2022-04-28 15:43:44 -0700

    Update get-cookie.sh

--- API-1.5/curl/get-cookie.sh
+++ API-1.5/curl/get-cookie.sh
@@ -5,7 +5,7 @@ email=${rs_email:='your@email.com'}     # RS User Account
 pass=${rs_pswd:='yourpassword'}         # RS User Password
 acct_id=${rs_acct:='12345'}             # RS Account ID
 
-echo "Authenticating and retrieving cookie for $email @ $acct_id.  Saving cookie to file 'mycookie'"
+echo "Authenticating and retrieving cookie for $email @ $acct_id. Saving cookie to file 'mycookie'"
 
 ### Execute API Call to retrieve cookie and save it to mycookie
 account_href="/api/accounts/$acct_id"
